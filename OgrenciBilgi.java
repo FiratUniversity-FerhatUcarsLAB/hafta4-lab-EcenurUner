4d83fef92ccd74510055586a3942c96be5799f47
EcenurUner <250541078@firat.edu.tr>
2025-10-24 19:49:44 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [Ecenur Üner]
+ * Ogrenci No: [250541078]
+ * Tarih: [24.10.2025]
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -45,7 +45,35 @@ public class OgrenciBilgi {
         System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
         
         // COZUMUNUZU BURAYA YAZIN
-        
+        import java.util.Scanner;
+  public class OgrenciBilgi {
+    public static void main(String[] args) {
+
+        //değişken alınıyor
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Adınızı girin: ");
+        String ad = input.nextLine();
+
+        System.out.print("Soyadınızı girin: ");
+        String soyad = input.nextLine();
+
+        System.out.print("Öğrenci No girin: ");
+        int  ogrenci_no = input.nextInt();
+
+        System.out.print("Yaşınızı girin: ");
+        int yas = input.nextInt();
+
+        System.out.print("GPA (0.00-4.00): ");
+        float gpa = input.nextFloat();
+
+        System.out.printf("===Ogrenci Bilgileri===");
+        System.out.printf("\nAd Soyad: %s %s" ,ad,soyad);
+        System.out.printf("\nOgrenci No: %d" ,ogrenci_no);
+        System.out.printf("\nYas: %d" ,yas);
+        System.out.printf("\nGPA: %.2f" ,gpa);
+    }
+}
         
         
         
